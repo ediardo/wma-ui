b3745c2e56dd4f6708478fbb48db2e5999f481cd
Eddie Ramirez <djedi.r@gmail.com>
2019-03-26 22:41:27 -0700

    Added Virgin Australia

--- src/airlines.js
+++ src/airlines.js
@@ -6,6 +6,7 @@ export const airlines = {
   AFR: "Air France",
   SWA: "Southwest Airlines",
   THY: "Turkish Airlines",
+  VOZ: "Virgin Australia",
   EZY: "easyJet",
   ANZ: "Air New Zealand",
   AUA: "Austrian Airlines",
