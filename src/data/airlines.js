0f25c6b452ca401089c20d95b36c6bb853944897
Eddie Ramirez <djedi.r@gmail.com>
2019-03-29 10:48:43 -0700

    Countries dataset. Airlines with countries

---
+++ src/data/airlines.js
@@ -0,0 +1,232 @@
+export const airlines = [
+  {
+    icao: "AAL",
+    name: "American Airlines",
+    country: "USA"
+  },
+  {
+    icao: "DAL",
+    name: "Delta Air Lines",
+    country: "USA"
+  },
+  {
+    icao: "UAL",
+    name: "United Airlines",
+    country: "USA"
+  },
+  {
+    icao: "DLH",
+    name: "Lufthansa",
+    country: "GER"
+  },
+  {
+    icao: "AFR",
+    name: "Air France",
+    country: "FRA"
+  },
+  {
+    icao: "SWA",
+    name: "Southwest Airlines",
+    country: "USA"
+  },
+  {
+    icao: "THY",
+    name: "Turkish Airlines",
+    country: "TUR"
+  },
+  {
+    icao: "VOZ",
+    name: "Virgin Australia",
+    country: "AUS"
+  },
+  {
+    icao: "EZY",
+    name: "easyJet",
+    country: "CHE"
+  },
+  {
+    icao: "ANZ",
+    name: "Air New Zealand",
+    country: "N"
+  },
+  {
+    icao: "AUA",
+    name: "Austrian Airlines",
+    country: "AUS"
+  },
+  {
+    icao: "AVA",
+    name: "Avianca",
+    country: "COL"
+  },
+  {
+    icao: "ACA",
+    name: "Air Canada",
+    country: "CAN"
+  },
+  {
+    icao: "AIC",
+    name: "Air India",
+    country: "IND"
+  },
+  {
+    icao: "CCA",
+    name: "Air China",
+    country: "CHN"
+  },
+  {
+    icao: "QTR",
+    name: "Qatar Airways",
+    country: "QAT"
+  },
+  {
+    icao: "QFA",
+    name: "Qantas",
+    country: "AUS"
+  },
+  {
+    icao: "BAW",
+    name: "British Airways",
+    country: "GBR"
+  },
+  {
+    icao: "IBE",
+    name: "Iberia Airlines",
+    country: "ESP"
+  },
+  {
+    icao: "KLM",
+    name: "KLM Royal Dutch Airlines",
+    country: "NLD"
+  },
+  {
+    icao: "CSN",
+    name: "China Southern Airlines",
+    country: "CHN"
+  },
+  {
+    icao: "ARG",
+    name: "Aerolineas Argentinas",
+    country: "ARG"
+  },
+  {
+    icao: "KAL",
+    name: "Korean Air",
+    country: "KOR"
+  },
+  {
+    icao: "CSH",
+    name: "Shanghai Airlines",
+    country: "CHN"
+  },
+  {
+    icao: "ETD",
+    name: "Etihad Airways",
+    country: "ARE"
+  },
+  {
+    icao: "UAE",
+    name: "Emirates",
+    country: "ARE"
+  },
+  {
+    icao: "EIN",
+    name: "Aer Lingus",
+    country: "IRL"
+  },
+  {
+    icao: "VIR",
+    name: "Virgin Atlantic Airways",
+    country: "GBR"
+  },
+  {
+    icao: "ANA",
+    name: "All Nippon Airways",
+    country: "JPN"
+  },
+  {
+    icao: "WJA",
+    name: "WestJet",
+    country: "CAN"
+  },
+  {
+    icao: "HAL",
+    name: "Hawaiian Airlines",
+    country: "USA"
+  },
+  {
+    icao: "KAC",
+    name: "Kuwait Airways",
+    country: "KWT"
+  },
+  {
+    icao: "ASA",
+    name: "Alaska Airlines",
+    country: "USA"
+  },
+  {
+    icao: "AMX",
+    name: "Aeromexico",
+    country: "MEX"
+  },
+  {
+    icao: "JAL",
+    name: "Japan Airlines",
+    country: "JAP"
+  },
+  {
+    icao: "AAR",
+    name: "Asiana Airlines",
+    country: "KOR"
+  },
+  {
+    icao: "MSR",
+    name: "Egyptair",
+    country: "EGY"
+  },
+  {
+    icao: "RYR",
+    name: "Ryanair",
+    country: "IRL"
+  },
+  {
+    icao: "IBU",
+    name: "Indigo",
+    country: "IND"
+  },
+  {
+    icao: "FFT",
+    name: "Frontier Airlines",
+    country: "USA"
+  },
+  {
+    icao: "ETH",
+    name: "Ethiopian Airlines",
+    country: "ETH"
+  },
+  {
+    icao: "VOI",
+    name: "Volaris",
+    country: "MEX"
+  },
+  {
+    icao: "SWR",
+    name: "Swiss International Air Lines",
+    country: "che"
+  },
+  {
+    icao: "SAS",
+    name: "Scandinavian Airlines",
+    country: "SWE"
+  },
+  {
+    icao: "AXM",
+    name: "AirAsia",
+    country: "MYS"
+  },
+  {
+    icao: "JBU",
+    name: "JetBlue Airways",
+    country: "USA"
+  }
+];
